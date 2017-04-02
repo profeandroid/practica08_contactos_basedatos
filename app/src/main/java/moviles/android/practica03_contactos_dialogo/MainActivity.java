217d3990719957c4f033238251fc2d74eddac8c8
admin <admin@gmail.com>
2017-04-02 19:39:09 +0200

    para recuperar

--- app/src/main/java/moviles/android/practica03_contactos_dialogo/MainActivity.java
+++ app/src/main/java/moviles/android/practica03_contactos_dialogo/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends Activity {
 		this.startActivity(intent);
 	}
 	public void ver(View v){
-		//commit 1
+		//commit 2
 		Intent intent=new Intent(this,ListadoActivity.class);
 		this.startActivity(intent);
 	}
